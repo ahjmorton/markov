e4c142deabeed9161d8455b3c6b7f21269496dd3
Andrew Morton <ahjmorton@unknown-b8-f6-b1-11-0d-51.home>
2012-08-25 20:10:49 +0100

    Got it doing something weird

--- src/rand-markov.c
+++ src/rand-markov.c
@@ -13,16 +13,23 @@ typedef struct
 
 typedef struct markov_state {
 
-    int value;
+    char value;
     int path_count;
     markov_path * paths;
 } markov_state;
 
-markov_state * find_state(markov_state * root, int count) {
+static markov_state * find_state(markov_state * root, int count, int value) {
+    int i;
+    for(i = 0; i < count; i++) {
+        markov_state * current = root + i;
+        if(current->value == value) {
+            return current;
+        }
+    }
     return NULL;
 }
 
-int path_sort(const void * left, const void * right) {
+static int path_sort(const void * left, const void * right) {
     markov_path * leftPath = (markov_path *)left;
     markov_path * rightPath = (markov_path *)right;
     if(leftPath->range < rightPath->range) {
@@ -36,6 +43,19 @@ int path_sort(const void * left, const void * right) {
     }
 }
 
+static markov_path * find_path(markov_path * paths, int count, float range) {
+    int i;
+    for(i = 0; i < count; i++) {
+        markov_path * path = paths + i;
+        if(path->range > range) {
+            return path;
+        }
+    }
+    // Take the last one, presumbly
+    return paths + (count - 1);
+    
+}
+
 int main(void) {
     int i;
     int total = 0;
@@ -47,7 +67,7 @@ int main(void) {
         char next;
         float transition;
         sscanf(buffer, "%c:%c:%f", &state, &next, &transition);
-        markov_state * mstate = find_state(root, total);
+        markov_state * mstate = find_state(root, total, state);
         if(mstate == NULL) {
            ++total;
            root = (markov_state *)realloc(root, total * sizeof(markov_state));
@@ -56,7 +76,7 @@ int main(void) {
            mstate->paths = NULL;
            mstate->path_count = 0;
         }
-        markov_state * mnext = find_state(root, total);
+        markov_state * mnext = find_state(root, total, next);
         if(mnext == NULL) {
            ++total;
            root = (markov_state *)realloc(root, total * sizeof(markov_state));
@@ -70,7 +90,9 @@ int main(void) {
         markov_path * new_path = mstate->paths + (mstate->path_count - 1);
         new_path->range = transition;
         new_path->transition = mnext;
+        printf("Set %c to switch to %c at %f\n", mstate->value, mnext->value, transition);
     }
+    printf("Generated data structure\n");
     for(i = 0; i < total; i++) {
         markov_state * sort_state = root + i;
         qsort(sort_state->paths,
@@ -79,6 +101,15 @@ int main(void) {
               &path_sort);
               
     }
-    
+    printf("Done sorting\n");
+    int start = rand() % total;
+    markov_state * current = root + start;
+    for(i = 0; i < 100; i++) {
+        fputc(current->value, stdout);
+        float next_range = (float)(rand() % 101) / 100;
+        markov_path * path_to_follow = find_path(current->paths, current->path_count, next_range); 
+        current = path_to_follow->transition;
+            
+    }
     return 0;
 }
