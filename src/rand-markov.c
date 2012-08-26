448d17ffba633b7170e428171a0e87db5e3c39aa
Andrew Morton <ahjmorton@gmail.com>
2012-08-26 15:47:02 +0100

    Fixed rand-markov to behave correctly

--- src/rand-markov.c
+++ src/rand-markov.c
@@ -74,7 +74,7 @@ int main(void) {
         next = (int)c_next;
         markov_state * mstate = find_state(root, total, state);
         if(mstate == NULL) {
-           ++total;
+           total++;
            root = (markov_state *)realloc(root, total * sizeof(markov_state));
            mstate = root + (total - 1);
            mstate->value = state;
@@ -83,13 +83,13 @@ int main(void) {
         }
         markov_state * mnext = find_state(root, total, next);
         if(mnext == NULL) {
-           ++total;
-           printf("%i * %i = %i\n", total, sizeof(markov_state), total * sizeof(markov_state));
+           total++;
            root = (markov_state *)realloc(root, total * sizeof(markov_state));
            mnext = root + (total - 1);
            mnext->value = next;
            mnext->paths = NULL;
            mnext->path_count = 0;
+           mstate = find_state(root, total, state);
         }
         (mstate->path_count)++;
         mstate->paths = (markov_path *)realloc(mstate->paths, (mstate->path_count) * sizeof(markov_path));
@@ -106,22 +106,17 @@ int main(void) {
               
     }
     int start = rand() % total;
-    printf("start is %i \n", start);
     markov_state * current = root + start;
-    printf("current is %c \n", current->value);
-    for(i = 0; i < total; i++) {
-        markov_state * fuckingCheck = current + i;
-        printf("Fucking check at %p : %c %p %i\n", fuckingCheck, fuckingCheck->value, fuckingCheck->paths, fuckingCheck->path_count);
-    }
     for(i = 0; i < 100; i++) {
         fputc(current->value, stdout);
         float next_range = (float)(rand() % 101) / 100;
-        printf(" next range is %f\n", next_range);
-        printf("paths here is %p and count is %i\n", current->paths, current->path_count);
         markov_path * path_to_follow = find_path(current->paths, current->path_count, next_range);
-        
         current = find_state(root, total, path_to_follow->transition);
             
+    } 
+    for(i = 0; i < total; i++) { 
+        free((root + i)->paths);
     }
+    free(root);
     return 0;
 }
