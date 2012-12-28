2c31d67aea3f396a60b7f99a91fa3acbdde29e57
Andrew Morton <ahjmorton@gmail.com>
2012-12-28 14:06:33 +0000

    Added licenses to the headings of the test scripts

--- test/py/correct_test.py
+++ test/py/correct_test.py
@@ -1,4 +1,20 @@
 #!/usr/bin/python
+#
+#This file is part of markov.
+#
+#markov is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#markov is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with markov.  If not, see <http://www.gnu.org/licenses/>.
+#
 
 from sys import argv
 import subprocess
