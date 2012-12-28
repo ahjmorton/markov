6178049dc6161b3cc3052976bda938077ea5feff
Andrew Morton <ahjmorton@gmail.com>
2012-12-28 13:51:42 +0000

    Made the requirements to equal 1 a lot less stringent on the correctness test

--- test/py/correct_test.py
+++ test/py/correct_test.py
@@ -9,7 +9,7 @@ input_path = argv[2]
 with open(input_path, "r") as input_file : 
     corpus_output = subprocess.check_output([command], stdin=input_file)
 
-def process_output(output_text) :
+def _process_output(output_text) :
     result = dict()
     if isinstance(output_text, str) :
         output_text = output_text.split("\n")
@@ -28,7 +28,7 @@ def process_output(output_text) :
     return result
 
 
-corpus_result = process_output(corpus_output)
+corpus_result = _process_output(corpus_output)
 
 total_prob = dict()
 
@@ -41,5 +41,17 @@ for first in corpus_result :
             total_prob[second] = float(0)
         total_prob[first] = total_prob[first] + first_corpus[second]
 
+
+"""Returns whether the number is between 0.99999999995 and 1.00000000005.
+
+The reason for this is because for large inputs we may be bumping up against
+whatever precision limitations are present on the platform we're operating 
+on, so assume "as close to one as makes no odds" is good enough.
+
+If you need more than 10 decimal places worth of accuracy then patches welcome. 
+"""
+def _is_one(num) :
+    return 0.99999999995 <= num <= 1.00000000005
+
 for character in total_prob :
-    assert total_prob[character] == float(1.0), "Value for {0} should be 1.0 but is {1!r}".format(character, total_prob[character])
+    assert _is_one(total_prob[character]), "Value for {0} should be close to 1.0 but is {1!r}".format(character, total_prob[character])
