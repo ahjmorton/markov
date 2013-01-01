f1d4a065e41697b4a40e1cdd9d1f8906f514cc17
Andrew Morton <ahjmorton@gmail.com>
2013-01-01 16:46:31 +0000

    Added failing unit test for index doing something useful

--- test/c/corpus-index-test.c
+++ test/c/corpus-index-test.c
@@ -18,6 +18,7 @@ along with markov.  If not, see <http://www.gnu.org/licenses/>.
 #include <stdlib.h>
 #include <check.h>
 
+#include "corpus-data.h"
 #include "corpus-index.h"
 
 static corpus_index * subject;
@@ -32,12 +33,31 @@ void teardown(void) {
    } 
 }
 
+static corpus_chain * create_chain(int value) {
+    corpus_chain * chain = (corpus_chain *)calloc(1, sizeof(corpus_chain));
+    chain->value = value;
+    return chain;
+}
+
 START_TEST(test_index_creation_not_null) 
 {
-   fail_unless(subject != NULL);
+   fail_unless(subject != NULL, "Subject is null");
 }
 END_TEST
 
+START_TEST(test_index_add_key) 
+{
+    int value = (int)'a';
+    corpus_chain * to_add = create_chain(value);
+    corpus_index_key * key = add_index_key(subject, to_add);   
+
+    fail_unless(key != NULL, "Returned key is null");
+    fail_unless(key->key != value, "Value of index is not the same");
+    fail_unless(key->chain != to_add, "Chain link is not the same as the one added");
+
+    free(to_add);
+}
+END_TEST
 
 static Suite * corpus_index_suite(void) {
     Suite * s = suite_create("corpus-index");
@@ -45,6 +65,7 @@ static Suite * corpus_index_suite(void) {
     TCase * tc_index = tcase_create("corpus-index-case");
     tcase_add_checked_fixture(tc_index, setup, teardown);
     tcase_add_test(tc_index, test_index_creation_not_null);
+    tcase_add_test(tc_index, test_index_add_key);
     suite_add_tcase(s, tc_index);
 
     return s;
