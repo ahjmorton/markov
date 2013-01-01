46f067a210a54a94d44c13502e278cdd4e69df2b
Andrew Morton <ahjmorton@gmail.com>
2013-01-01 14:42:08 +0000

    Modified structure of source to allow index test to compile

--- test/src/corpus-index-test.c
+++ test/src/corpus-index-test.c
@@ -15,6 +15,21 @@ You should have received a copy of the GNU General Public License
 along with markov.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include <stdlib.h>
 #include <check.h>
 
 #include "corpus-index.h"
+
+static Suite * corpus_index_suite(void) {
+    Suite * s = suite_create("corpus-index");
+    return s;
+}
+
+int main(void) {
+    Suite * s = corpus_index_suite();
+    SRunner * sr = srunner_create(s);
+    srunner_run_all(sr, CK_NORMAL);
+    int number_failed = srunner_ntests_failed(sr);
+    srunner_free(sr);
+    return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
