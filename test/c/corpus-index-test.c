f056971eeed69fd5897b6e98eb1cfb26f497f7b1
Andrew Morton <ahjmorton@gmail.com>
2013-01-02 20:40:54 +0000

    Added tests for finding the key

--- test/c/corpus-index-test.c
+++ test/c/corpus-index-test.c
@@ -39,6 +39,12 @@ static corpus_chain * create_chain(int value) {
     return chain;
 }
 
+static corpus_node * create_node(int value) {
+    corpus_node * node = (corpus_node *)calloc(1, sizeof(corpus_node));
+    node->other = value;
+    return node;
+}
+
 START_TEST(test_index_creation_not_null) 
 {
    fail_unless(subject != NULL, "Subject is null");
@@ -47,13 +53,13 @@ END_TEST
 
 START_TEST(test_index_add_key) 
 {
-    int value = (int)'a';
-    corpus_chain * to_add = create_chain(value);
-    corpus_index_key * key = add_index_key(subject, to_add);   
+    int key = (int)'a';
+    corpus_chain * to_add = create_chain(key);
+    corpus_index_key * index_key = add_index_key(subject, to_add);   
 
-    fail_unless(key != NULL, "Returned key is null");
-    fail_unless(key->key == value, "Value of index is not the same");
-    fail_unless(key->chain == to_add, "Chain link is not the same as the one added");
+    fail_unless(index_key != NULL, "Returned key is null");
+    fail_unless(index_key->key == key, "Value of index is not the same");
+    fail_unless(index_key->chain == to_add, "Chain link is not the same as the one added");
 
     free(to_add);
 }
@@ -61,14 +67,14 @@ END_TEST
 
 START_TEST(test_index_find_key_present) 
 {
-    int value = (int)'a';
-    corpus_chain * to_add = create_chain(value);
+    int key = (int)'a';
+    corpus_chain * to_add = create_chain(key);
     add_index_key(subject, to_add);   
 
-    corpus_index_key * key = find_index_key(subject, value);
-    fail_unless(key != NULL, "Found key was null");
-    fail_unless(key->key == value, "Value of index is not the same");
-    fail_unless(key->chain == to_add, "Chain link is not the same as the one added");
+    corpus_index_key * index_key = find_index_key(subject, key);
+    fail_unless(index_key != NULL, "Found key was null");
+    fail_unless(index_key->key == key, "Key of index is not the same");
+    fail_unless(index_key->chain == to_add, "Chain link is not the same as the one added");
 
     free(to_add);
 }
@@ -76,10 +82,61 @@ END_TEST
 
 START_TEST(test_index_find_key_missing) 
 {
-    int value = (int)'a';
-    corpus_index_key * key = find_index_key(subject, value);
-    fail_unless(key == NULL, "Found a key in an empty index");
+    int key = (int)'a';
+    corpus_index_key * index_key = find_index_key(subject, key);
+    fail_unless(index_key == NULL, "Found a key in an empty index");
 }
+END_TEST
+
+START_TEST(test_index_add_value) 
+{
+    int key = (int)'a';
+    int value = (int)'b';
+    corpus_chain * to_add = create_chain(key);
+    corpus_index_key * index_key = add_index_key(subject, to_add);   
+  
+    corpus_node * to_add_value = create_node(value);
+    corpus_index_value * index_value = add_index_value(index_key, to_add_value);
+    fail_unless(index_value != NULL, "Index value is null");
+    fail_unless(index_value->value == value, "Index value is not the same as input");
+    fail_unless(index_value->node == to_add_value, "Node pointer is wrong");
+
+    free(to_add);      
+    free(to_add_value);
+}
+END_TEST
+
+START_TEST(test_index_find_value_present) 
+{
+    int key = (int)'a';
+    int value = (int)'b';
+    corpus_chain * to_add = create_chain(key);
+    corpus_index_key * index_key = add_index_key(subject, to_add);   
+  
+    corpus_node * to_add_value = create_node(value);
+    add_index_value(index_key, to_add_value);
+    corpus_index_value * index_value = find_index_value(index_key, value); 
+    fail_unless(index_value != NULL, "Index value is null");
+    fail_unless(index_value->value == value, "Index value is not the same as input");
+    fail_unless(index_value->node == to_add_value, "Node pointer is wrong");
+
+    free(to_add);      
+    free(to_add_value);
+}
+END_TEST
+
+START_TEST(test_index_find_value_missing) 
+{
+     int key = (int)'a';
+    int value = (int)'b';
+    corpus_chain * to_add = create_chain(key);
+    corpus_index_key * index_key = add_index_key(subject, to_add);   
+  
+    corpus_index_value * index_value = find_index_value(index_key, value); 
+    fail_unless(index_value == NULL, "Key is missing and should be null");
+ 
+    free(to_add);
+} 
 END_TEST
 
 static Suite * corpus_index_suite(void) {
@@ -91,6 +148,9 @@ static Suite * corpus_index_suite(void) {
     tcase_add_test(tc_index, test_index_add_key);
     tcase_add_test(tc_index, test_index_find_key_present);
     tcase_add_test(tc_index, test_index_find_key_missing);
+    tcase_add_test(tc_index, test_index_add_value);
+    tcase_add_test(tc_index, test_index_find_value_present);
+    tcase_add_test(tc_index, test_index_find_value_missing);
     suite_add_tcase(s, tc_index);
 
     return s;
